78337efd6c5c4be493f489c3d163cfc2989beb3c
Fabien Raymond <voortexxx@gmail.com>
2018-11-14 11:23:19 +0100

    Updates

---
+++ precache-manifest.4dd581a8a9f662dd1a10b068a91c890e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c5bcf5e982c7bc47dc",
+    "url": "/reactchat/static/js/runtime~main.c3c5bcf5.js"
+  },
+  {
+    "revision": "e4f824a355465d1e294e",
+    "url": "/reactchat/static/js/main.e4f824a3.chunk.js"
+  },
+  {
+    "revision": "9271c0a6a42fd4b93f39",
+    "url": "/reactchat/static/js/1.9271c0a6.chunk.js"
+  },
+  {
+    "revision": "e4f824a355465d1e294e",
+    "url": "/reactchat/static/css/main.b557e88d.chunk.css"
+  },
+  {
+    "revision": "b02abb4e74f1ba1ae14ae2d6933b8cd2",
+    "url": "/reactchat/index.html"
+  }
+];
